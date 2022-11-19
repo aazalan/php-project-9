2fa376dbb3685e50b777d703c59ce12792527096
Alina Abdul <alinka140901@gmail.com>
2022-11-19 16:32:21 +0300

    add test router

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->addErrorMiddleware(true, true, true);
+
+$app->get(
+    '/', function (Request $request, Response $response) {
+        $response->getBody()->write('hi there');
+        return $response;
+    }
+);
+
+$app->run();
